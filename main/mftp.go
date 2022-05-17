ef4b6cc56ed965b8fd4d60a7fb1fa16d6e0bfe23
Zheng Zhou <bevischou@outlook.com>
2022-05-17 21:44:34 +0800

    refactor ftp

--- main/mftp.go
+++ main/mftp.go
@@ -9,16 +9,18 @@ import (
 func main() {
 	var ftp FtpUtils
 	// test
-	fileName := "./folder/test.txt"
+	localFileName := "test.txt"
+	remoteFileName := "tes.txt"
+	newLocalFileName := "te.txt"
 	// clientPath := "./rubbishfile/"
 	ftp.Construct(os.Args[1])
 	fmt.Println("start to upload")
-	ftp.UploadFile(fileName, "", "") //假定当前路径minisql，将test.txt传入到  /home/tyz
+	ftp.UploadFile(localFileName, remoteFileName) //假定当前路径minisql，将test.txt传入到  /home/tyz
 	fmt.Println("upload test finished")
 	fmt.Println("start to download file")
-	ftp.DownloadFile("", fileName, "./main/")
+	ftp.DownloadFile(remoteFileName, newLocalFileName, false)
 	fmt.Println("finish downloading")
 	fmt.Println("start to delete file")
-	// ftp.DeleteFile(fileName, "") // 删除/home/tyz下的test.txt
+	ftp.DeleteFile(remoteFileName) // 删除/home/tyz下的test.txt
 	fmt.Println("finish deleting")
 }
