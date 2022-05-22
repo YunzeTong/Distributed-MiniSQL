d7b5a13fe4927559e3c981598cebc76dedf03a0f
Yunze Tong <tongyunze2001@163.com>
2022-05-22 11:24:32 +0800

    finish hint's print format

--- client/client.go
+++ client/client.go
@@ -35,7 +35,7 @@ func (client *Client) Init(masterIP string) {
 
 	rpcMas, err := rpc.DialHTTP("tcp", masterIP+MASTER_PORT)
 	if err != nil {
-		fmt.Printf("[client]connect error: %v", err)
+		fmt.Printf("SYSTEM HINT>>> client connect error: %v", err)
 	}
 	client.rpcMaster = rpcMas
 
@@ -44,7 +44,7 @@ func (client *Client) Init(masterIP string) {
 func (client *Client) Run() {
 	for {
 		// read a complete sql from keyboard, store it in input
-		fmt.Println("新消息>>>请输入你想要执行的SQL语句: ")
+		fmt.Println("new message>>> input your SQL command: ")
 		input := ""      // user's complete input
 		part_input := "" // part of the input, all of them compose input
 
@@ -61,16 +61,17 @@ func (client *Client) Run() {
 
 		input = strings.Trim(input, " ")
 
-		fmt.Println("[风神翼龙test in loop]input: " + input)
+		// fmt.Println("[风神翼龙test in loop]input: " + input)
 
 		if input == "quit" {
 			//客户端直接退出
+			fmt.Println("new message>>> You choose to quit, bye!")
 			break
 		}
 
 		op, table, index, err := client.preprocessInput(input)
 		if err != nil {
-			fmt.Printf("[input format error] %v\n", err)
+			fmt.Printf("INPUT FORMAT ERROR>>> %v\n", err)
 			continue
 		}
 		switch op {
@@ -79,38 +80,39 @@ func (client *Client) Run() {
 			args, ip := TableArgs{Table: table, SQL: input}, ""
 			call, err := TimeoutRPC(client.rpcMaster.Go("Master.CreateTable", &args, &ip, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("timeout")
+				fmt.Println("SYSTEM HINT>>> timeout")
 			}
 			if call.Error != nil {
-				fmt.Println("[from master to client]create table failed")
+				fmt.Println("RESULT>>> create table failed")
 			} else {
-				fmt.Println("create table succeed, table in ip: " + ip)
+				fmt.Println("RESULT>>> create table succeed, table in ip: " + ip)
 			}
 		case DROP_TBL:
 			// call Master.DropTable rpc
 			args, dummy := TableArgs{Table: table, SQL: input}, false
 			call, err := TimeoutRPC(client.rpcMaster.Go("Master.DropTable", &args, &dummy, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("timeout")
+				fmt.Println("SYSTEM HINT>>> timeout")
 			}
 			if call.Error != nil {
-				fmt.Println("[error]drop table failed")
+				fmt.Println("RESULT>>> drop table failed")
+			} else {
+				fmt.Println("RESULT>>> drop table succeed")
 			}
 		case SHOW_TBL:
 			// 把所有region的table名显示出来
 			// TODO: call Master.ShowTables and format output
-			fmt.Println("show all the tables in region")
 			var dummyArgs bool
 			var tables []string
 			// tables = make([]string, 0)
 			call, err := TimeoutRPC(client.rpcMaster.Go("Master.ShowTables", &dummyArgs, &tables, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("timeout")
+				fmt.Println("SYSTEM HINT>>> timeout")
 			}
 			if call.Error != nil {
-				fmt.Println("[error]show tables failed")
+				fmt.Println("RESULT>>> show tables failed")
 			} else {
-				fmt.Println("tables in region:\n---------------")
+				fmt.Println("RESULT>>> tables in region:\n---------------")
 				for _, table := range tables {
 					fmt.Printf("|  %v  |\n", table)
 				}
@@ -123,41 +125,40 @@ func (client *Client) Run() {
 			args, ip := IndexArgs{Index: index, Table: table, SQL: input}, ""
 			call, err := TimeoutRPC(client.rpcMaster.Go("Master.CreateIndex", &args, &ip, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("timeout")
+				fmt.Println("RESULT>>> timeout")
 			}
 			if call.Error != nil {
-				fmt.Println("[error]create indexes failed")
+				fmt.Println("RESULT>>> create indexes failed")
 			} else {
-				fmt.Printf("create index succeed on table %v in %v\n", table, ip)
+				fmt.Printf("RESULT>>> create index succeed on table %v in %v\n", table, ip)
 			}
 		case DROP_IDX:
 			// TODO: call Master.DropIndex
 			args, dummy := IndexArgs{Index: index, SQL: input}, false
 			call, err := TimeoutRPC(client.rpcMaster.Go("Master.DropIndex", &args, &dummy, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("timeout")
+				fmt.Println("SYSTEM HINT>>> timeout")
 			}
 			if call.Error != nil {
-				fmt.Println("[error]drop indexes failed")
+				fmt.Println("RESULT>>> drop indexes failed")
 			} else {
-				fmt.Println("drop index succeed")
+				fmt.Println("RESULT>>> drop indexes succeed")
 			}
 		case SHOW_IDX:
 			// TODO: call Master.ShowIndices and format output
-			fmt.Println("show all the indexes in region")
 			var dummyArgs bool
 			var indices map[string]string
 			// indices = make(map[string]string)
 			call, err := TimeoutRPC(client.rpcMaster.Go("Master.ShowIndices", &dummyArgs, &indices, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("timeout")
+				fmt.Println("SYSTEM HINT>>> timeout")
 			}
 			if call.Error != nil {
-				fmt.Println("[error]show indices failed")
+				fmt.Println("RESULT>>> show indices failed")
 			} else {
-				fmt.Println("indices in region:\n|  index  |  table  |")
+				fmt.Println("RESULT>>> indices in region:\n|    index    |    table    |")
 				for index, table := range indices {
-					fmt.Printf("|  %v  |  %v  |\n", index, table)
+					fmt.Printf("|   %v   |   %v   |\n", index, table)
 				}
 				fmt.Println("---------------")
 			}
@@ -167,44 +168,41 @@ func (client *Client) Run() {
 			if !ok {
 				ip = client.updateCache(table)
 				if ip == "" {
-					fmt.Println("can't find the corresponding ip in cache")
+					// fmt.Println("can't find the corresponding ip in cache")
 					break
 				}
 			} else {
-				fmt.Println("find corresponding ip in table-ip map: " + ip)
+				fmt.Println("SYSTEM HINT>>> first phase: find corresponding ip in table-ip cache: " + ip)
 			}
 			// call Region.Process rpc with ip var
 			result := ""
 			// obtain regionRPC
 			rpcRegion, ok := client.rpcRegionMap[ip]
 			if !ok {
-				fmt.Printf("region not in RPCcache, add it to map")
+				// fmt.Printf("region not in RPCcache, add it to map")
 				rpcRegion, err = rpc.DialHTTP("tcp", ip+REGION_PORT)
 				if err != nil {
-					fmt.Println(err)
-					fmt.Println("fail to connect to region: " + ip)
-					fmt.Println("IP is new but can't connect")
+					// fmt.Println("fail to connect to region: " + ip)
+					// fmt.Println("IP is new but can't connect")
 					delete(client.ipCache, table)
 					break
 				} else {
-					// client.ipCache[table] = ip
-					_, ok := client.ipCache[table]
-					if !ok {
-						fmt.Println("[正常情况下不会出现此打印]no ip-table cache in map")
-					}
+					// _, ok := client.ipCache[table]
+					// if !ok {
+					// 	fmt.Println("[正常情况下不会出现此打印]no ip-table cache in map")
+					// }
 					client.rpcRegionMap[ip] = rpcRegion
 				}
-			} else {
-				fmt.Println("[最终可删除]first phase: find corresponding rpc in rpc map")
 			}
 
 			call, err := TimeoutRPC(rpcRegion.Go("Region.Process", &input, &result, nil), TIMEOUT)
 			if err != nil {
-				fmt.Println("[region process]timeout")
+				fmt.Println("SYSTEM HINT>>> region process timeout")
 			}
 			if call.Error != nil {
-				//TODO: 这里的error有可能是sql错误导致或者ip旧了rpcRegion拿不到导致，先放一放
-				fmt.Println("can't obtain result, maybe old ip or sql error")
+				//TODO: 这里的error有可能是sql错误导致或者ip旧了rpcRegion拿不到导致
+				fmt.Println("SYSTEM HINT>>> can't obtain result in first phase, maybe old ip or SQL command error")
+				fmt.Println("SYSTEM HINT>>> start second phase operation...")
 
 				// ip, rpcRegion is old，select for twice
 				// first delete old cache
@@ -212,32 +210,34 @@ func (client *Client) Run() {
 				delete(client.rpcRegionMap, ip)
 				new_ip := client.updateCache(table) // obtain newest ip
 				if new_ip == "" {
-					fmt.Println("can't find the ip which table in")
+					// fmt.Println("can't find the ip which table in")
 					break
 				}
 				// obtain newest rpcRegion and update map
 				new_rpcRegion, err := rpc.DialHTTP("tcp", new_ip+REGION_PORT)
 				if err != nil {
-					fmt.Printf("[client]fail to connect to region: " + ip)
+					fmt.Printf("SYSTEM HINT>>> fail to connect to region: " + ip)
 					delete(client.ipCache, table)
 					break
 				}
 				// call Region.Process rpc again
 				call, err := TimeoutRPC(new_rpcRegion.Go("Region.Process", &input, &result, nil), TIMEOUT)
 				if err != nil {
-					fmt.Println("[no cache and region process]timeout")
+					fmt.Println("SYSTEM HINT>>> region process timeout")
 					break
 				}
 				if call.Error != nil {
-					fmt.Println("can't obatin result, maybe input is error")
+					fmt.Println("SYSTEM HINT>>> can't obatin result, maybe input is error")
 					break
 				}
 				// fmt.Println("result:\n" + result)
 				client.ipCache[table] = new_ip
 				client.rpcRegionMap[new_ip] = new_rpcRegion
-				fmt.Println("[最终不一定非得删除]update ip: " + ip + " and add " + new_ip + " to iptablemap")
+				fmt.Println("SYSTEM HINT>>> update ip: " + ip + " and add " + new_ip + " to iptablemap")
 			}
-			fmt.Println("result:\n" + result)
+			if result != "" {
+				fmt.Println("RESULT>>> \n" + result)
+			}
 		}
 
 	}
@@ -323,14 +323,12 @@ func (client *Client) preprocessInput(input string) (TableOp, string, string, er
 			for i := 0; i < len(words); i++ {
 				if words[i] == "from" && i != (len(words)-1) {
 					table = words[i+1]
-					fmt.Println("[最终可删]operation: select, table: " + table)
 					break
 				}
 			}
 		} else if words[0] == "insert" || words[0] == "delete" {
 			if len(words) >= 3 {
 				table = words[2]
-				fmt.Println("[最终可删]operation: insert or delete, table: " + table)
 			}
 		} else {
 			err = errors.New("can't recoginize your command")
@@ -350,11 +348,11 @@ func (client *Client) updateCache(table string) string {
 	// call Master.TableIP rpc
 	call, err := TimeoutRPC(client.rpcMaster.Go("Master.TableIP", &table, &ip, nil), TIMEOUT)
 	if err != nil {
-		fmt.Println("[update cache]timeout")
+		fmt.Println("SYSTEM HINT>>> during update cache, timeout")
 		return ip
 	}
 	if call.Error != nil {
-		fmt.Println("[table invalid]update cache failed")
+		fmt.Println("SYSTEM HINT>>> table invalid, can't update cache")
 		return ip
 	}
 	client.ipCache[table] = ip
